0abf3cb36b01e5c5f634cfdd272a305631d39b70
Vaibhav Tomar <vibhudon321@gmail.com>
2025-02-23 11:13:09 +0530

    Your commit message

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import './Contact.css'
-// import { FaEnvelope ,FaLocationDot, FaPhone} from "react-icons/fa6";
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import './Contact.css';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1f58ncl', 'template_tjyhe1a', form.current, 'r-R6eTVigPSxeTUPY')
+      .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to send message. Please try again.');
+      });
+  };
+
   return (
     <>
       <div className="contact" id='contact'>
@@ -10,45 +25,44 @@ const Contact = () => {
           <h2>Get In Touch</h2>
         </div>
         <div className="contact_info">
-        <div className="contact_left">
+          <div className="contact_left">
             <h2>Lets Talk Business</h2>
-            <p>Feel free to reach out to me for any inquiries or collaboration. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.
-            </p>
+            <p>Feel free to reach out to me for any inquiries or collaboration. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
             <div className="contact_details">
               <div className="detail">
-              {/* <FaEnvelope /> */}
+                {/* <FaEnvelope /> */}
                 <p>webwandersolutions</p>
               </div>
               <div className="detail">
-              {/* <FaPhone /> */}
+                {/* <FaPhone /> */}
                 <p>+91 8766219989</p>
               </div>
               <div className="detail">
-              {/* <FaLocationDot /> */}
+                {/* <FaLocationDot /> */}
                 <p>Chakrata Road , Chukkuwala , Dehradun</p>
               </div>
             </div>
-        </div>
-        <div className="contact_right">
-        <div className="contact_input">
-          <div className="text">
-            <input type="text" placeholder='Email' />
           </div>
-          <div className="text">
-            <input type="text" placeholder='Subject' />
+          <div className="contact_right">
+            <form ref={form} onSubmit={sendEmail} className="contact_input">
+              <div className="text">
+                <input type="text" name="user_email" placeholder='Email' required />
+              </div>
+              <div className="text">
+                <input type="text" name="subject" placeholder='Subject' required />
+              </div>
+              <div className="message">
+                <textarea name="message" cols="30" rows="10" placeholder='Message' required></textarea>
+              </div>
+              <div className="btn">
+                <button type="submit">Send Message</button>
+              </div>
+            </form>
           </div>
-          <div className="message">
-            <textarea name="Message"  cols="30" rows="10">Message</textarea>
-          </div>
-          <div className="btn">
-            <button>Send Message</button>
-          </div>
-        </div>
-        </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
